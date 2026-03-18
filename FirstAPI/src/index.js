0afc7d8032e52308a63c054361f6859a0393636b
Ricardo Franco dos Santos <ricardofrancosantos@gmail.com>
2026-03-18 09:36:10 -0300

    feat : made server start

--- FirstAPI/src/index.js
+++ FirstAPI/src/index.js
@@ -5,5 +5,5 @@ const server = http.createServer((request, response) => {
 });
 
 server.listen(3000, () =>
-  console.log("Server started at http://localhost:3000/"),
+  console.log("🔥Server started at http://localhost:3000/"),
 );
