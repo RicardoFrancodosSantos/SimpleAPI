5ee8cd24b864c1dba7e06d9c9dd5135d1f403ef5
Ricardo Franco dos Santos <ricardofrancosantos@gmail.com>
2026-03-18 09:12:16 -0300

    feat : create server and server listening

--- FirstAPI/index.js
+++ FirstAPI/index.js
@@ -1,1 +1,5 @@
 const http = require("http");
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("<h1>Hello World!</h1>");
+});
